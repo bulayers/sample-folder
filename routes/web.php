be2a9f7f14dc66a01e4c896150a4ac1038c08daa
CHRISTIAN EARL A. BONA <2202546@cc.usls.edu>
2025-03-13 11:34:24 +0800

    Displaying categories data with using laravel directives

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 Route::get('/login', 'App\Http\Controllers\LoginController@Index');
 
 Route::get('/hello', 'App\Http\Controllers\HelloController@Index');
+
+Route::get('/categories', 'App\Http\Controllers\CategoryController@index');
